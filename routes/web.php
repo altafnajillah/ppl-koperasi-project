22b156483d5561730ee12c4f0397fbe4faa279e7
Minarti07 <134377125+Minarti07@users.noreply.github.com>
2025-10-13 10:38:26 +0700

    web.php

--- routes/web.php
+++ routes/web.php
@@ -6,53 +6,27 @@ use App\Http\Controllers\SimpananController;
 use App\Http\Controllers\PinjamanController;
 
 
+//BiodataController
+Route::get('/biodata/create', [BiodataController::class, 'create'])->name('biodata.create');
+Route::post('/biodata', [BiodataController::class, 'store'])->name('biodata.store');
+Route::get('/biodata/{biodata}', [BiodataController::class, 'show'])->name('biodata.show');
+Route::get('/biodata/{biodata}/edit', [BiodataController::class, 'edit'])->name('biodata.edit');
+Route::put('/biodata/{biodata}', [BiodataController::class, 'update'])->name('biodata.update');
+Route::delete('/biodata/{biodata}', [BiodataController::class, 'destroy'])->name('biodata.destroy');
 
-// BiodataController
-// ====================
+// Route SimpananController
+Route::get('/simpanan/create', [SimpananController::class, 'create'])->name('simpanan.create');
+Route::post('/simpanan', [SimpananController::class, 'store'])->name('simpanan.store');
+Route::get('/simpanan/{biodata}', [SimpananController::class, 'index'])->name('simpanan.index');
+Route::delete('/simpanan/{simpanan}', [SimpananController::class, 'destroy'])->name('simpanan.destroy');
 
-// Tampilkan biodata berdasarkan ID
-Route::get('/biodata/{biodata}', [BiodataController::class, 'show']);
-
-// Form tambah biodata
-Route::get('/biodata/create', [BiodataController::class, 'create']);
-Route::post('/biodata', [BiodataController::class, 'store']);
-
-// Form edit biodata
-Route::get('/biodata/{biodata}/edit', [BiodataController::class, 'edit']);
-Route::put('/biodata/{biodata}', [BiodataController::class, 'update']);
-
-// Hapus biodata
-Route::delete('/biodata/{biodata}', [BiodataController::class, 'destroy']);
-
-
-// SimpananController
-// ====================
-
-// Form tambah simpanan
-Route::get('/simpanan/create', [SimpananController::class, 'create']);
-Route::post('/simpanan', [SimpananController::class, 'store']);
-
-// Tampilkan simpanan milik anggota tertentu (berdasarkan ID anggota)
-Route::get('/simpanan/{anggota}', [SimpananController::class, 'index']);
-
-// Hapus simpanan (berdasarkan ID simpanan)
-Route::delete('/simpanan/{simpanan}', [SimpananController::class, 'destroy']);
+//Route PinjamanController
+Route::get('/pinjaman/create', [PinjamanController::class, 'create'])->name('pinjaman.create');
+Route::post('/pinjaman', [PinjamanController::class, 'store'])->name('pinjaman.store');
+Route::get('/pinjaman/{biodata}', [PinjamanController::class, 'index'])->name('pinjaman.index');
+Route::post('/pinjaman/{pinjaman}/approve', [PinjamanController::class, 'approve'])->name('pinjaman.approve');
 
 
-// PinjamanController
-// ====================
-
-// Form ajukan pinjaman
-Route::get('/pinjaman/create', [PinjamanController::class, 'create']);
-Route::post('/pinjaman', [PinjamanController::class, 'store']);
-
-// Setujui pinjaman (berdasarkan ID pinjaman)
-Route::get('/pinjaman/approve/{pinjaman}', [PinjamanController::class, 'approve']);
-
-// Lihat pinjaman per anggota (berdasarkan ID anggota)
-Route::get('/pinjaman/{anggota}', [PinjamanController::class, 'index']);
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
